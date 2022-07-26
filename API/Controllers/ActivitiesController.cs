e9b4ab85623b905649157467d15b331ce662de36
tkmusic <68860474+tkmusic@users.noreply.github.com>
2022-07-26 15:36:59 +0200

    Added the Details and Create Handler

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -15,7 +15,13 @@ namespace API.Controllers
 
         [HttpGet("{id}")] //activities/id
         public async Task<ActionResult<Activity?>> GetActivity(Guid id){
-            return Ok();
+            return await Mediator!.Send(new Details.Query{Id = id});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            return Ok(await Mediator!.Send(new Create.Command {Activity = activity}));
         }
     }
 }
