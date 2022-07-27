a205934c768e82656c36baf136401f83d27f89da
tkmusic <68860474+tkmusic@users.noreply.github.com>
2022-07-27 14:39:31 +0200

    Added the Edit Handler Added AutoMapper

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -21,7 +21,14 @@ namespace API.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateActivity(Activity activity)
         {
-            return Ok(await Mediator!.Send(new Create.Command {Activity = activity}));
+            return Ok(await Mediator!.Send(new Create.Command{Activity = activity}));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            return Ok(await Mediator!.Send(new Edit.Command{Activity = activity}));
         }
     }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -15,6 +15,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Persistence;
+using AutoMapper;
+using Application.Core;
 
 namespace API
 {
@@ -48,6 +50,7 @@ namespace API
                 });
             });
             services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
